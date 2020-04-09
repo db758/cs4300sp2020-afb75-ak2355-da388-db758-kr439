a4a5ca055d902927aafacf9662da71cb69fc0f3b
Divya <da388@cornell.edu>
2020-04-09 10:29:25 -0400

    changed title

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "CUpids"
+net_id = "Net Ids: afb75, ak2355, da388, db758, kr439"
 
 @irsystem.route('/', methods=['GET'])
 def search():
