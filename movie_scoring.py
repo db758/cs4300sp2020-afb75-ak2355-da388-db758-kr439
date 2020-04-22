444b3fa0ef9925c7e6631132e6309dc408d461ce
Asma Khan <ak2355@cornell.edu>
2020-04-22 16:14:06 -0500

    genre score working

--- movie_scoring.py
+++ movie_scoring.py
@@ -4,7 +4,7 @@ import csv
 
 def getMovieAndFoodWords(user1_movies, user2_movies):
 	# List of all movie names inputted by both users 
-	input_movie_list = getMovieNames(user1_movies) + getMovieNames(user1_movies)
+	input_movie_list = getMovieNames(user1_movies) + getMovieNames(user2_movies)
 
 	# Read in the csv of movies
 	movies = list(csv.DictReader(open('new.csv')))
@@ -17,47 +17,35 @@ def getMovieAndFoodWords(user1_movies, user2_movies):
 	movie_to_attributes = {}
 
 	# Generate movie_to_genre, movie_to_categories, and movie_to_attributes dictionaries
-	i=0
 	for each_movie in movies:
-		if each_movie["Genres"] != "":
+		#CHANGE THIS SO THAT WE CAN STILL DO COSINE
+		if each_movie["Genres"] == "":
+			movie_to_genre[str(each_movie["Title"]).lower()] = []
+		else:
 			movie_to_genre[str(each_movie["Title"]).lower()] = eval(each_movie["Genres"])
-			i += 1
-			movie_to_categories[str(each_movie["Title"]).lower()] = eval(each_movie["categories"])
-			movie_to_attributes[str(each_movie["Title"]).lower()] = eval(each_movie["attributes"])
-	
-	print(i)
+		movie_to_categories[str(each_movie["Title"]).lower()] = eval(each_movie["categories"])
+		movie_to_attributes[str(each_movie["Title"]).lower()] = eval(each_movie["attributes"])
+		## ADD A PLOT DICTIONARY for COSSINE
+
 
 	#All the genres in the movies that the user inputted	
-	input_movie_genres = getMovieGenres(input_movie_list, movie_to_genre)
+	input_movie_genres = getMovieGenres (input_movie_list, movie_to_genre)
 	#All unique genres in the movies that the user inputted	
 	unique_input_movie_genres = list(set(input_movie_genres))
+	
 	#List of all movies
 	all_movies = list(movie_to_genre.keys())
 
-	#Gets the movie ranking. Highest scored movie is the last element in the list.
-	movie_rank = getMovieRanking(unique_input_movie_genres,input_movie_genres, all_movies, movie_to_genre)
+	#Genre Scores of All movies
+	genre_score_array = getGenreScore(unique_input_movie_genres,input_movie_genres, input_movie_list, all_movies, movie_to_genre)
+	# for i in range(0,len(genre_score_array)):
+	# 	if genre_score_array[i] > 0:
+	# 		print(all_movies[i], genre_score_array[i] )
 
-	#Goes through the movie ranking starting from the back
-	#If the movie is not one of the inputted movies, then returns the movie
-	for i in range(1,len(all_movies)):
-		index_movie = movie_rank[len(all_movies)-i]
-		if all_movies[index_movie] not in input_movie_list:
-			return [all_movies[index_movie], movie_to_categories[all_movies[index_movie]], movie_to_attributes[all_movies[index_movie]]]
-	return["","",""]
 
-def getAllMovies():
-	"""Used for drop down for movie.
-	
-	Returns: a list with all the movie titles"""
+	##CHANGE -- currently returning nothing
+	return ["","",""] 
 
-	movies = list(csv.DictReader(open('new.csv')))
-	all_movies = []
-	for each_movie in movies:
-		all_movies.append(str(each_movie["Title"].lower()))
-	
-	with open("all_movies.txt", "w") as output:
-		for mov in all_movies:
-			output.write(mov+ '\n')
 
 def getMovieNames(movie_name):
 	"""Given a the user input of movie names (which are split by commas)
@@ -77,9 +65,10 @@ def getMovieGenres (input_movie_list, movie_to_genre):
 		input_movie_genres = input_movie_genres +  movie_to_genre[each_movie]
 	return input_movie_genres
 
-def getMovieRanking(unique_input_movie_genres,input_movie_genres, all_movies, movie_to_genre):
-	"""Return a list of indexes where the first index is the lowest scored movie based on genres.
-	This is where we do genre frequency * genres in a movie"""
+def getGenreScore(unique_input_movie_genres,input_movie_genres,input_movie_list, all_movies, movie_to_genre):
+	"""Return a the genre score"""
+	if input_movie_genres == "":
+		return 0
 
 	#Create a numpy matric that is movie_genres by all movies
 	genresBYmovies = np.zeros((len(unique_input_movie_genres), len(all_movies)))
@@ -97,8 +86,32 @@ def getMovieRanking(unique_input_movie_genres,input_movie_genres, all_movies, mo
 	for i in range(0,len(unique_input_movie_genres)):
 		genre_count.append(input_movie_genres.count(unique_input_movie_genres[i]))
 
-	#Multiplies the numpy matrix and the counts to get a scoring and the argsorts it
-	return np.argsort(np.sum((genresBYmovies.transpose())*genre_count, axis=1))
+	movie_scoring = np.sum((genresBYmovies.transpose())*genre_count, axis=1)
+	return movie_scoring
+	# #Multiplies the numpy matrix and the counts to get a scoring and the argsorts it
+	# movie_rank =  np.argsort(movie_scoring)
+
+	# #Goes through the movie ranking starting from the back
+	# #If the movie is not one of the inputted movies, then returns the movie
+	# for i in range(1,len(all_movies)):
+	# 	index_movie = movie_rank[len(all_movies)-i]
+	# 	if all_movies[index_movie] not in input_movie_list:
+	# 		return movie_scoring[index_movie]
+	# 		#return [all_movies[index_movie], movie_to_categories[all_movies[index_movie]], movie_to_attributes[all_movies[index_movie]]]
+
+def getAllMovies():
+	"""Used for drop down for movie.
+	
+	Returns: a list with all the movie titles"""
+
+	movies = list(csv.DictReader(open('new.csv')))
+	all_movies = []
+	for each_movie in movies:
+		all_movies.append(str(each_movie["Title"].lower()))
+	
+	with open("all_movies.txt", "w") as output:
+		for mov in all_movies:
+			output.write(mov+ '\n')
 
 
 getAllMovies()
