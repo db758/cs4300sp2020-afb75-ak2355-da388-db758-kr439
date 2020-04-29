261d18412e975e1d32b2da37fc5fae579c93af4d
Divya <da388@cornell.edu>
2020-04-29 15:24:20 -0400

    working on movie

--- movie_scoring.py
+++ movie_scoring.py
@@ -23,7 +23,7 @@ class MovieScoring(object):
 		#Initialize the Cosine parts
 		# self.movies_tokens = self.token(self.movie_to_summaries)
 		# self.inv_idx = self.buildInvertedIndex(self.movies_tokens)
-		# self.idf = self.compute_idf(self.inv_idx, len(self.movie_to_summaries), min_df=10, max_df_ratio=0.1)
+		# self.idf = self.compute_idf(self.inv_idx, len(self.movie_to_summaries), min_df=10, max_df_ratio=0.9)
 		# self.inv_idx = {key: val for key, val in self.inv_idx.items() if key in self.idf}
 		# self.doc_norms = self.compute_doc_norms(self.inv_idx, self.idf, len(self.movie_to_summaries))
 
@@ -81,7 +81,7 @@ class MovieScoring(object):
 			
 			movie_to_categories[str(each_movie["Title"]).lower()] = eval(each_movie["categories"])
 			movie_to_attributes[str(each_movie["Title"]).lower()] = eval(each_movie["attributes"])
-			#movie_to_summaries[str(each_movie["Title"]).lower()] = (each_movie["Plot"])
+			movie_to_summaries[str(each_movie["Title"]).lower()] = (each_movie["Plot"])
 
 			if each_movie["Cast"] == "":
 				movie_to_cast[str(each_movie["Title"]).lower()] = []
@@ -292,6 +292,8 @@ class MovieScoring(object):
 	def token(self, movie_summaries):
 		result = {}
 		for mov in movie_summaries:
+			# temp = re.sub(r'[^\w\s]','',movie_summaries[mov].lower())
+			# temp = re.split(r'[\s.,;]+', movie_summaries[mov].lower())
 			result[mov] = movie_summaries[mov].lower().split(" ")
 		return result
 
@@ -501,8 +503,8 @@ class MovieScoring(object):
 
 # print("Method")
 # movieClass = MovieScoring()
-# movieClass.getCSVs()
-#print(movieClass.inv_idx)
+# # movieClass.getCSVs()
+# print(len(movieClass.inv_idx.keys()))
 
 
 #CASES:
